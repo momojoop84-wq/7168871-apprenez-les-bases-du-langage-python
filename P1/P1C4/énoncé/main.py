86e9bd3d06f4fcc342f08223f815d4fd998765e9
momojoop84-wq <momojoop84@gmail.com>
2026-03-23 14:33:08 +0100

    Initialisation et F" main.py
    Initiliser et afficher ma varible avec le f"

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,16 @@
+"""
+Créez une variable nommée nom et attribuez-y votre nom.
+Créez une variable nommée age et attribuez-y votre âge.
+Affichez une phrase contenant votre nom et votre âge en utilisant les variables que vous venez de créer. Par exemple, "Je m'appelle Jean et j'ai 25 ans." (Utilisez le f-string)
+Modifiez la valeur de la variable age pour ajouter 10 ans à votre âge actuel.
+Affichez à nouveau la phrase contenant votre nom et votre âge en utilisant les variables mises à jour. Par exemple, "Je m'appelle Jean et j'ai 35 ans maintenant." (Utilisez le f-string)
+"""
 # Écrivez votre code ici !
+ #initialisation d'une variable
+Age=25
+#Afficher la variable avec le f"
+print(f"Je m'appelle Momo Joop et j'ai {age} ans.")
+#Ajouter 10 à ma varible
+Age=Age+10
+#Réafficher la varibale avec la nouvelle valeur en utilisant le f"
+print(f"Je m'appelle Momo Joop et j'ai {age} ans.)
