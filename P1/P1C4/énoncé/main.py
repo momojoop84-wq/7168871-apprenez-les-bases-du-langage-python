38e9c725ebb464091d22cb32befb609a655ce371
momojoop84-wq <momojoop84@gmail.com>
2026-03-23 13:49:37 +0100

    Utilisations des variables main.py
    Utilisations des variables et affichage avec f"

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+# crivez votre code ici ! Créez une variable nommée nom et attribuez-y votre nom.
+"""
+Créez une variable nommée age et attribuez-y votre âge
+Affichez une phrase contenant votre nom et votre âge en utilisant les variables que vous venez de créer. Par exemple, "Je m'appelle Jean et j'ai 25 ans." (Utilisez le f-string)
+Modifiez la valeur de la variable age pour ajouter 10 ans à votre âge actuel.
+Affichez à nouveau la phrase contenant votre nom et votre âge en utilisant les variables mises à jour. Par exemple, "Je m'appelle Jean et j'ai 35 ans maintenant." (Utilisez le f-string)
+"""
+
+age=25
+print(f"Je m'appelle Momo Joop et j'ai{age}ans.")
+age=age+10
+print(f"Je m'appelle Momo Joop et j'ai {age}ans.")
