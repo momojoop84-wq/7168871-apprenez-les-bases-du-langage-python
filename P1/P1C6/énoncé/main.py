fedb2f6d28fca87bed17161f03de398c27169e08
momojoop84-wq <momojoop84@gmail.com>
2026-03-24 11:49:10 +0100

    travailler avec les ensembles  main.py
    créer et manipuler une liste

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,18 @@
 # Écrivez votre code ici !
+"""
+Créez une liste nommée fruits contenant les éléments "pomme", "banane" et "orange".
+Ajoutez "kiwi" à la liste fruits.
+Supprimez "orange" de la liste fruits.
+Modifiez le deuxième élément de la liste fruits en "ananas".
+Affichez la longueur de la liste fruits.
+Triez la liste fruits par ordre alphabétique
+Affichez la liste. (N'oubliez pas d'afficher la liste pour que les tests passent)
+
+"""
+fruits=["pomme","banane","orange"]
+fruits.append("kiwi")
+del fruits[2]
+fruits[1]=["ananas"]
+len(fruits)
+fruits.sort
+print(fruits)
