91a7b62dd1447e8f01f24184f8113c9de0bb8abd
momojoop84-wq <momojoop84@gmail.com>
2026-03-24 13:59:45 +0100

    travailler avec les dict() main.py
    manipulation de donnée avec les dictionnaires

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,17 @@
 # Écrivez votre code ici !
+"""
+Un dévoilement de dictionnaireCrétins fruits avec les clés "pomme", "banane"Et "orange", et les valeurs "rouge", "jaune"Et "orange".
+AjoutezLa clé "kiwi"Avec la valeur "vert"au dictionnaire fruits.
+Accédez à la valeur correspondant à la clé "banane"et stockez-la dans une variable appelée couleur_banane.
+Modifiez la valeur associée à la clé "pomme"Verser "vert".
+Supprimez la clé "banane"du dictionnaire fruits.
+AffichezLes clés restantes dans le dictionnaire.
+"""
+fruits={
+  "pomme":"rouge","banane":"jaune","orange":"orange"
+}
+fruits["kiwi"]="vert"
+couleur=fruits.get("banane")
+fruits["pomme"]="vert"
+del fruits["banane"]
+print(fruits)
