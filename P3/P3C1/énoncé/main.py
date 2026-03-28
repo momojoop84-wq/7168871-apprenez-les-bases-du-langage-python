64bfe805887f965a2e220332c5fbf0523699f471
momojoop84-wq <momojoop84@gmail.com>
2026-03-28 11:51:11 +0100

    Import_main.py
    Importer un module

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,1 +1,10 @@
 # Ecrivez votre code ici !
+import operations
+
+if values == int:
+  somme=operations.somme(3,5)
+  print(somme)
+
+if values == int
+  produit=operations.multiplication(8,2)
+  print()
