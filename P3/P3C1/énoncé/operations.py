37e3985b89d11744ce4d7598c65186af3d6881c9
momojoop84-wq <momojoop84@gmail.com>
2026-03-28 11:01:34 +0100

    Modules_operations.py
    créer un module

--- P3/P3C1/énoncé/operations.py
+++ P3/P3C1/énoncé/operations.py
@@ -1,1 +1,13 @@
 # Écrivez votre code ici !
+
+def salaire_mensuel(salaire) :
+    salaire=salaire/12
+    return salaire
+     
+def salaire_hebdomadaire(salaire) :
+    salaire_hebdomadaire=salaire/4
+    return salaire_hebdomadaire
+
+def salaire_horaire(salaire_hebdomadaire,heures_de_travaille) :
+    salaire_horaire=salaire_hebdomadaire/heures_de_travaille
+    return salaire_horaire
