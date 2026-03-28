a076e6126b0c47a99d7ca5c152878b120b26002e
momojoop84-wq <momojoop84@gmail.com>
2026-03-28 12:09:30 +0100

    Type_of_DATA_main.py
    Classer des données avec leur type

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,11 @@
 # Écrivez votre code ici !
+Name=input("Quelle est votre nom ? ")
+Age=input("Quelle âge avez vous ? ")
+Length=input("Combien mesurez vous ? ")
+Is_student= input("Répondez uniquement par oui ou non svp : êtes vous étudiant ? ")
+
+print(f" Vous êtes {Name}, vous avez {Age}, vous mesurez {Length} et à la question si vous êtes étudiant c'est {Is_student} .")
+Type(Name)
+Type(Age)
+Type(Length)
+Type(Is_student)
